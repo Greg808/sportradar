c1d6cb66afb84e8ce8f64db5b1b4803fe27cc02a
Gregor Weindl <g.weindl@pixelpoems.com>
2020-03-23 15:14:09 +0100

    add bootstrap file, db credentials

---
+++ boostrap.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+/* set your db credentials */
+$configuration = [
+    'db_dsn' => 'mysql:host=localhost;dbname=sportradar',
+    'db_u' => 'root',
+    'db_p' => 'dont-use-in-production'
+];
+
+
