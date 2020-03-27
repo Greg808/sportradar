8f52582c5fa214ec548755a0fed763c65d591896
Gregor Weindl <g.weindl@pixelpoems.com>
2020-03-27 14:56:05 +0100

    created eventsTableBodyInclude

---
+++ eventsTableBodyInclude.php
@@ -0,0 +1,9 @@
+<tr>
+    <td><?php echo $event->getPlayDay(); ?></td>
+    <td><?php echo $event->getPlayDate(); ?></td>
+    <td><?php echo $event->getPlayTime(); ?></td>
+    <td><?php echo $event->getSportTitle(); ?></td>
+    <td><?php echo $event->getTeamOneTitle(); ?></td>
+    <td><?php echo $event->getTeamTwoTitle(); ?></td>
+    <td><a href="">Show More</a></td>
+</tr>
