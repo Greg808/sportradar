a34ed9a45c3ffd16e7b3669779e423955f4ec6d7
Gregor Weindl <g.weindl@pixelpoems.com>
2020-03-27 10:02:34 +0100

    created EventsLoader methods: getEvents, createEventModelFromData

--- app/src/Service/EventsLoader.php
+++ app/src/Service/EventsLoader.php
@@ -30,17 +30,17 @@ class EventsLoader
         return $events;
     }
 
-    private function creatEventModelFromData(array $eventData)
+    private function creatEventModelFromData(array $eventData): EventModel
     {
         $event = new EventModel();
         $event->setId($eventData['id']);
-        $event->setTitle($eventData['event_title']);
+        $event->setTitle($eventData['title']);
         $event->setEventStart($eventData['event_start']);
         $event->setEventEnd($eventData['event_end']);
         $event->setIsCanceled($eventData['is_canceled']);
         $event->setSportTitle($eventData['sport_title']);
         $event->setTeamOneTitle($eventData['team_one_title']);
-        $event->setTeamTwoId($eventData['team_two_title']);
+        $event->setTeamTwoTitle($eventData['team_two_title']);
         $event->setCountryTitle($eventData['country_title']);
         return $event;
     }
